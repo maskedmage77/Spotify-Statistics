f3884902b9f310ac008361c18b14ec2909dee3b6
maskedmage77 <maskedmage77@gmail.com>
2022-08-08 20:48:26 -0400

    added mantine theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { Button, Space, Title } from '@mantine/core';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <h1>Test</h1>
+       
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,6 +8,55 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      theme={{
+        colors: {
+          MaskyGreen: [
+            "#8EDF9D",
+            "#75D787",
+            "#5DD072",
+            "#48CA60",
+            "#38C251",
+            "#33B04A",
+            "#2EA043",
+            "#29903C",
+            "#258236",
+            "#227531"
+          ],
+          MaskyBlue: [
+            "#273345",
+            "#202A39",
+            "#1B2330",
+            "#161D28",
+            "#131821",
+            "#10141C",
+            "#0D1117",
+            "#0A0E12",
+            "#080B0F",
+            "#07090C",
+          ]
+        },
+        colorScheme: 'dark',
+        primaryColor: 'MaskyGreen',  
+        fontFamily: 'Jost, sans serif',
+        headings: {
+          fontFamily: 'Jost, sans serif',
+          sizes: {
+            h1: { fontSize: 64 },
+            h2: { fontSize: 48 },
+            h3: { fontSize: 32 },
+            h4: { fontSize: 24 },
+            h5: { fontSize: 20 },
+            h6: { fontSize: 16 },
+          },
+        },
+        spacing: { xs: 8, sm: 16, md: 24, lg: 32, xl: 40 },
+        radius: { sm: 4, md: 8, lg: 16, xl: 24 },
+        shadows: 'lg',
+        fontSizes: { xs: 12, sm: 14, md: 16, lg: 18, xl: 20 },
+      }}
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
